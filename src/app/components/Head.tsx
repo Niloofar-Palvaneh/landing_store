8a5c553290189861b4069cf014c181f750f2b251
Niloofar-Palvaneh <niloofarpalvaneh@gmail.com>
2025-06-05 17:38:58 +0330

    completed head and add images

--- src/app/components/Head.tsx
+++ src/app/components/Head.tsx
@@ -5,15 +5,17 @@ import PrimaryBTN from './PrimaryBTN'
 
 function Head() {
     return (
-        <div className="relative bg-[url('/imgs/head1.png')] bg-no-repeat bg-cover bg-center ">
-            <div className="p-4 text-white pt-32 pr-24">
-                <h1 className="text-5xl text-shadow-lg/30 tracking-wide  w-[25rem] leading-relaxed">{DRESS_FOR_THE_POSITION_YOU_ARE_STANDING_IN}</h1>
+        <div className="relative md:bg-[url('/imgs/head1.png')] bg-[url('/imgs/head2.png')] bg-no-repeat bg-cover md:bg-center bg-top ">
+            <div className="p-4 text-white pt-32 md:pr-24 pr:12 pb-32">
+                <h1 className="text-5xl text-shadow-lg/30 tracking-wide w-[25rem] leading-relaxed hidden md:flex">{DRESS_FOR_THE_POSITION_YOU_ARE_STANDING_IN}</h1>
                 <p className="mt-12 text-xl mb-8">{SPECIAL_COLLECTION_FOR_WOMEN_WHO_LIVE_UNIQUE}</p>
-                <PrimaryBTN title={VIEW_PRODUCTS} link="/"/>
+                <PrimaryBTN title={VIEW_PRODUCTS} link="/" />
             </div>
-            <div className="absolute inset-0 flex items-center justify-center">
-                <Image src="/imgs/circle.png" alt="عکس" width={100} height={100} className='w-[15rem]' />
+            <div className="absolute inset-0 items-center justify-center hidden md:flex">
+                <Image src="/imgs/circle.png" alt="عکس" width={100} height={100} className='w-[15rem] opacity-50' />
             </div>
+            <Image src="/imgs/HERO.png" alt="عکس" width={100} height={100} className='w-[10rem] absolute bottom-24 md:bottom-0 right-[35%] opacity-60 pb-4' />
+            <Image src="/imgs/HERO.png" alt="عکس" width={100} height={100} className='w-[10rem] absolute -bottom-24 left-[15%] opacity-60 hidden md:inline' />
         </div>
     )
 }
