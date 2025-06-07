a1267d608b3e561c0aa772c61a16f3594cd8f6bd
Niloofar-Palvaneh <niloofarpalvaneh@gmail.com>
2025-06-07 12:59:09 +0330

    category section created

--- sharedData.ts
+++ sharedData.ts
@@ -7,3 +7,10 @@ export const menuItems = [
     { id: 4, title: ABOUT_US },
     { id: 5, title: BRANCHES }
 ]
+export const categoryItems = [
+    { id: 1, name: "دامن", imgURL: "/imgs/cot.png" },
+    { id: 2, name: "کت", imgURL: "/imgs/daman.jpg" },
+    { id: 3, name: "مانتو", imgURL: "/imgs/manto.png" },
+    { id: 4, name: "شلوار", imgURL: "/imgs/shalvar.png" },
+    { id: 5, name: "اکسسوری", imgURL: "/imgs/axsesory.png" },
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Head from "./components/Head";
+import Category from "./components/Category";
 
 export default function Home() {
   return (
     <>
     <Head/>
+    <Category/>
       home page
     </>
   );
