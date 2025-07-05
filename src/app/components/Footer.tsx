5a37ba930c006afce479bf760d92941429ed4601
Niloofar-Palvaneh <niloofarpalvaneh@gmail.com>
2025-07-06 00:31:23 +0330

    translate footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,32 +1,36 @@
+"use client"
+
 import React from 'react'
-import { ABOUT_US, ADDRESS, CONTACT_INFORMATION, CONTACT_US, PHONE_NUMBER, PRODUCTS, QUICK_ACCESS } from '../../../words'
 import Image from 'next/image'
+import { useMyTranslation } from '@/hooks/useMyTranslation';
+import { TRANSLATION_KEYS } from '../../../i18nKeys';
 
 function Footer() {
+    const { t } = useMyTranslation();
     return (
         <>
             <footer className='grid md:grid-cols-4 grid-cols-2 p-4 md:px-12 md:py-8 bg-primary text-white mt-8'>
                 <ul className='flex gap-6 flex-col'>
-                    <li className='font-bold'>{CONTACT_INFORMATION}</li>
-                    <li>{PHONE_NUMBER}</li>
-                    <li>{ADDRESS}</li>
+                    <li className='font-bold'>{t(TRANSLATION_KEYS.CONTACT_INFORMATION)}</li>
+                    <li>{t(TRANSLATION_KEYS.PHONE_NUMBER)}</li>
+                    <li>{t(TRANSLATION_KEYS.ADDRESS)}</li>
                 </ul>
                 <ul className=' gap-6 flex-col hidden md:flex'>
-                    <li className='font-bold'>{QUICK_ACCESS}</li>
-                    <li>{ABOUT_US}</li>
-                    <li>{CONTACT_US}</li>
-                    <li>{PRODUCTS}</li>
+                    <li className='font-bold'>{t(TRANSLATION_KEYS.QUICK_ACCESS)}</li>
+                    <li>{t(TRANSLATION_KEYS.ABOUT_US)}</li>
+                    <li>{t(TRANSLATION_KEYS.CONTACT_US)}</li>
+                    <li>{t(TRANSLATION_KEYS.PRODUCTS)}</li>
                 </ul>
                 <ul className=' gap-6 flex-col  hidden md:flex'>
-                    <li className='font-bold'>{QUICK_ACCESS}</li>
-                    <li>{ABOUT_US}</li>
-                    <li>{CONTACT_US}</li>
-                    <li>{PRODUCTS}</li>
-                    <li>{PRODUCTS}</li>
+                    <li className='font-bold'>{t(TRANSLATION_KEYS.QUICK_ACCESS)}</li>
+                    <li>{t(TRANSLATION_KEYS.ABOUT_US)}</li>
+                    <li>{t(TRANSLATION_KEYS.CONTACT_US)}</li>
+                    <li>{t(TRANSLATION_KEYS.PRODUCTS)}</li>
+                    <li>{t(TRANSLATION_KEYS.PRODUCTS)}</li>
                 </ul>
                 <ul className='flex gap-6 flex-col'>
-                    <li><Image alt='نماد اعتماد' width={100} height={100} src={"/imgs/footer-1.png"}/></li>
-                    <li><Image alt='نماد اعتماد' width={100} height={100} src={"/imgs/footer-2.png"}/></li>
+                    <li><Image alt='نماد اعتماد' width={100} height={100} src={"/imgs/footer-1.png"} /></li>
+                    <li><Image alt='نماد اعتماد' width={100} height={100} src={"/imgs/footer-2.png"} /></li>
                 </ul>
             </footer>
         </>
