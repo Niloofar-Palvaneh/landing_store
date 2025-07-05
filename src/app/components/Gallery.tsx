ac2dda065e85cc25b25c88dea2ae97edeef723e8
Niloofar-Palvaneh <niloofarpalvaneh@gmail.com>
2025-07-06 00:37:49 +0330

    translate gallery section texts

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,13 +1,16 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
-import PrimaryBTN from './PrimaryBTN'
 import { VIEW_PRODUCTS } from '../../../words'
 import SecondaryBTN from './SecondaryBTN'
+import { useMyTranslation } from '@/hooks/useMyTranslation';
+import { TRANSLATION_KEYS } from '../../../i18nKeys'
 
 function Gallery() {
+    const { t } = useMyTranslation();
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-6 mt-12 md:h-[1000px]">
-            {/* ستون چپ - دو عکس که ارتفاع کل ستون راست رو پر کنن */}
             <div className="grid grid-rows-2 gap-4 h-full">
                 <div className="w-full h-full relative group overflow-hidden">
                     <Image
@@ -18,8 +21,8 @@ function Gallery() {
                         className="w-full h-full object-cover"
                     />
                     <div className="absolute bottom-0 left-0 w-full h-full bg-white/15 backdrop-blur-md opacity-0 translate-y-full group-hover:opacity-100 group-hover:translate-y-0 transition-all flex items-center justify-center flex-col gap-8 cursor-pointer">
-                        <p className='text-4xl font-bold [text-shadow:_0px_3px_3px_#e1c019]'>استایلتو بساز</p>
-                        <SecondaryBTN link="/" title={VIEW_PRODUCTS} />
+                        <p className='text-4xl font-bold [text-shadow:_0px_3px_3px_#e1c019]'>{t(TRANSLATION_KEYS.CREATE_YOUR_STYLE)}</p>
+                        <SecondaryBTN link="/" title={t(TRANSLATION_KEYS.VIEW_PRODUCTS)} />
                     </div>
                 </div>
                 <div className="w-full h-full relative group overflow-hidden">
@@ -31,13 +34,12 @@ function Gallery() {
                         className="w-full h-full object-cover"
                     />
                     <div className="absolute bottom-0 left-0 w-full h-full bg-white/15 backdrop-blur-md opacity-0 translate-y-full group-hover:opacity-100 group-hover:translate-y-0 transition-all flex items-center justify-center flex-col gap-8 cursor-pointer">
-                        <p className='text-4xl font-bold [text-shadow:_0px_3px_3px_#e1c019]'>استایلتو بساز</p>
-                        <SecondaryBTN link="/" title={VIEW_PRODUCTS} />
+                        <p className='text-4xl font-bold [text-shadow:_0px_3px_3px_#e1c019]'>{t(TRANSLATION_KEYS.CREATE_YOUR_STYLE)}</p>
+                        <SecondaryBTN link="/"  title={t(TRANSLATION_KEYS.VIEW_PRODUCTS)} />
                     </div>
                 </div>
             </div>
 
-            {/* ستون راست - یک عکس بزرگ */}
             <div className="w-full h-full relative group overflow-hidden">
                 <Image
                     src="/imgs/gallery-3.png"
@@ -47,8 +49,8 @@ function Gallery() {
                     className="w-full h-full object-cover"
                 />
                 <div className="absolute bottom-0 left-0 w-full h-full bg-white/15 backdrop-blur-md opacity-0 translate-y-full group-hover:opacity-100 group-hover:translate-y-0 transition-all flex items-center justify-center flex-col gap-8 cursor-pointer">
-                    <p className='text-4xl font-bold [text-shadow:_0px_3px_3px_#e1c019]'>استایلتو بساز</p>
-                    <SecondaryBTN link="/" title={VIEW_PRODUCTS} />
+                    <p className='text-4xl font-bold [text-shadow:_0px_3px_3px_#e1c019]'>{t(TRANSLATION_KEYS.CREATE_YOUR_STYLE)}</p>
+                    <SecondaryBTN link="/"  title={t(TRANSLATION_KEYS.VIEW_PRODUCTS)} />
                 </div>
             </div>
         </div>
